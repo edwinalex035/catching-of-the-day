b5058b258c6887bbe60a2ae1ec8eed433662c47d
Edwin Sanchez <edwinalex035@gmail.com>
2020-05-10 22:57:54 -0600

    :art: Validating status in menu

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -4,6 +4,7 @@ import { formatPrice } from '../helpers';
 export default class Fish extends Component {
     render() {
         const { image, name, desc, price, status } = this.props.details;
+        const isAvailable = status === 'available';
         return (
             <li className="menu-fish">
                 <img src={ image }
@@ -13,7 +14,9 @@ export default class Fish extends Component {
                     <span className="price">{ formatPrice(price) }</span>
                 </h3>
                 <p>{ desc }</p>
-                <button>Add To Cart</button>
+                <button disabled={ !isAvailable }>
+                    { isAvailable ? 'Add To Order' : 'Sold Out'}
+                </button>
             </li>
         )
     }
