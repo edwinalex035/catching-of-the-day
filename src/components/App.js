8e29cb8293297da075268547db8b966346497937
Edwin Sanchez <edwinalex035@gmail.com>
2020-05-11 16:54:24 -0600

    :sparkles: Adding order feature

--- src/components/App.js
+++ src/components/App.js
@@ -25,6 +25,14 @@ export default class App extends Component {
         });
     }
 
+    addToOrder = (key) => {
+        const order = { ...this.state.order };
+        order[key] = order[key] + 1 || 1;
+        this.setState({
+            order
+        });
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
@@ -34,11 +42,14 @@ export default class App extends Component {
                         {
                             Object.keys(this.state.fishes)
                                 .map(key => <Fish key={ key }
-                                    details={ this.state.fishes[key] } />)
+                                    index={ key }
+                                    details={ this.state.fishes[key] }
+                                    addToOrder={ this.addToOrder } />)
                         }
                     </ul>                 
                 </div>
-                <Order />
+                <Order fishes={ this.state.fishes }
+                    order={ this.state.order } />
                 <Inventory addFish={ this.addFish }
                     loadSampleFishes = { this.loadSampleFishes } />
             </div>
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import { formatPrice } from '../helpers';
 
 export default class Fish extends Component {
+
+    handleClick = () => {
+        this.props.addToOrder(this.props.index);
+    }
+
     render() {
         const { image, name, desc, price, status } = this.props.details;
         const isAvailable = status === 'available';
@@ -14,7 +19,7 @@ export default class Fish extends Component {
                     <span className="price">{ formatPrice(price) }</span>
                 </h3>
                 <p>{ desc }</p>
-                <button disabled={ !isAvailable }>
+                <button disabled={ !isAvailable } onClick={ this.handleClick }>
                     { isAvailable ? 'Add To Order' : 'Sold Out'}
                 </button>
             </li>
