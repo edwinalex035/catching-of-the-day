9b8d3df3cc55a210111b0160ea56af056d2cc95e
Edwin Sanchez <edwinalex035@gmail.com>
2020-05-10 22:22:19 -0600

    :sparkles: Adding fishes to inventory

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,27 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 export default class App extends Component {
+    state = {
+        fishes: {},
+        order: {}
+    }
+
+    addFish = (fish) => {
+        const fishes = { ...this.state.fishes };
+        fishes[`fish${Date.now()}`] = fish;
+        this.setState({
+            fishes
+        });
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
-                    <Header tagline="Fresh Seafood Market" />                    
+                    <Header />                    
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish={ this.addFish } />
             </div>
         )
     }
