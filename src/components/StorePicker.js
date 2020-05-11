b3a1aed51f5a14f57c31edfc1e57cc5415907f8a
Edwin Sanchez <edwinalex035@gmail.com>
2020-05-10 21:43:21 -0600

    :sparkles: Adding routing from store to app

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,23 @@
 import React, { Component } from 'react';
+import { getFunName } from '../helpers';
 
 export default class StorePicker extends Component {
+  myInput = React.createRef(); 
+
+  goToStore = (event) => {
+    event.preventDefault();
+    const storeName = this.myInput.current.value;
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector"
+        onSubmit={ this.goToStore }>
         <h2>Please Enter A Store</h2>
-        <input type="text" name="" id="" required placeholder="Store Name" />
+        <input type="text" name="" id="" required placeholder="Store Name"
+          defaultValue={ getFunName() }
+          ref={ this.myInput } />
         <button type="submit">Visit Store →</button>
       </form>
     )
